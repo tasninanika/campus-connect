6ddfcd4d0e6f04d7323c3b8e082a9502125094d1
anikatasnin <jarintasnin09@gmail.com>
2024-12-13 10:22:25 +0000

    updated alumni_announcement

--- alumni/alumni_announcement.php
+++ alumni/alumni_announcement.php
@@ -311,12 +311,12 @@
       while ($rows = mysqli_fetch_assoc($query)) {
         $announcement_title = $rows['title'];
         $content = $rows['content'];
-        $posted_date = $rows['created_at'];} ?>
+        $posted_date = $rows['created_at']; ?>
             <ol >
             <!-- 1 -->
             <li class="border-l-2 border-[#797DFC]">
                 <div class="md:flex flex-start">
-                <div class="bg-[#797DFC] w-6 h-6 flex items-center justify-center rounded-full -ml-2.5">
+                <div class="bg-[#797DFC] w-10 h-6 flex items-center justify-center rounded-full -ml-3">
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" class="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                     <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
                     </svg>
@@ -331,6 +331,9 @@
                 </div>
             </li>
             </ol>
+            <?php
+      }
+    }?>
             <!-- ====== announcement Section End -->
           </div>
         </main>
@@ -347,5 +350,5 @@
 	} else {
 		header('location:login.php?deactivate');
 	}
-}
+
 ?>

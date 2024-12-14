7b11c38a79ada95f064674a8d80d079598ff9627
anikatasnin <jarintasnin09@gmail.com>
2024-12-14 20:35:05 +0000

    updated alumni_profile.php

--- alumni/alumni_profile.php
+++ alumni/alumni_profile.php
@@ -173,7 +173,7 @@
     <!-- header -->
     <div class="w-full h-screen relative flex flex-col">
         <!-- Desktop Header -->
-        <header class="sticky top-0 z-999 w-full items-center bg-white py-4 px-6 hidden sm:flex">
+        <header class="sticky top-0 z-999 w-full items-center shadow bg-white py-4 px-6 hidden sm:flex">
             <div class="w-1/2">
             <h1 class="mb-36 lg:mb-0 -ml-10 lg:-ml-0 lg:text-4xl font-garamond flex multicolor-text"><img src="images/logo (2).png" alt="" class="lg:w-14 w-14 -mt-1">CampusConnect</h1>
             </div>

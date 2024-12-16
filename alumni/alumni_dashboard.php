8d6e8fefec51ccfef10ddaf2f13bb2382e66e638
anikatasnin <jarintasnin09@gmail.com>
2024-12-16 14:24:37 +0000

    updated alumni_dashboard.php

--- alumni/alumni_dashboard.php
+++ alumni/alumni_dashboard.php
@@ -181,138 +181,125 @@
             <div class="w-1/2">
             <h1 class="mb-36 lg:mb-0 -ml-10 lg:-ml-0 lg:text-4xl font-garamond flex dark:text-white multicolor-text"><img src="images/logo (2).png" alt="" class="lg:w-14 w-14 -mt-1">CampusConnect</h1>
             </div>
-            <div class="flex justify-end gap-3 2xsm:gap-7">
-                <ul class="flex justify-end gap-2 2xsm:gap-4">
-                <li>
-                  <!-- Dark Mode Toggler -->
-  
-                  <!-- Dark Mode Toggler -->
-                </li>
-                  <!-- Notification Menu Area -->
-                  <li
-                    class="relative"
-                    x-data="{ dropdownOpen: false, notifying: true }"
-                    @click.outside="dropdownOpen = false"
+            <div class="w-1/2 flex justify-end gap-4">
+              <!-- Notification Dropdown -->
+              <div x-data="{ dropdownOpen: false, notifying: true }" class="relative">
+                <a
+                  class="relative flex h-9 w-9 items-center justify-center rounded-full top-2 border border-gray-300 bg-gray-100 hover:text-primary dark:border-strokedark dark:bg-meta-4 dark:text-white"
+                  href="#"
+                  @click.prevent="dropdownOpen = !dropdownOpen; notifying = false"
+                >
+                
+                  <!-- Notification Dot -->
+                  <span
+                    :class="!notifying && 'hidden'"
+                    class="absolute -top-0.5 right-0 z-1 h-2 w-2 rounded-full bg-red-500"
+                  >
+                    <span
+                      class="absolute -z-1 inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"
+                    ></span>
+                  </span>
+
+                  <!-- Bell Icon -->
+                  <svg
+                    class="fill-current duration-300 ease-in-out"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
                     >
-                    <a
-                      class="relative flex h-8.5 w-8.5 items-center justify-center rounded-full border-[0.5px] border-stroke bg-gray hover:text-primary dark:border-strokedark dark:bg-meta-4 dark:text-white"
-                      href="#"
-                      @click.prevent="dropdownOpen = ! dropdownOpen; notifying = false"
-                    >
-                      <span
-                        :class="!notifying && 'hidden'"
-                        class="absolute -top-0.5 right-0 z-1 h-2 w-2 rounded-full bg-meta-1"
+                    <path
+                      d="M16.1999 14.9343L15.6374 14.0624C15.5249 13.8937 15.4687 13.7249 15.4687 13.528V7.67803C15.4687 6.01865 14.7655 4.47178 13.4718 3.31865C12.4312 2.39053 11.0812 1.7999 9.64678 1.6874V1.1249C9.64678 0.787402 9.36553 0.478027 8.9999 0.478027C8.6624 0.478027 8.35303 0.759277 8.35303 1.1249V1.65928C8.29678 1.65928 8.24053 1.65928 8.18428 1.6874C4.92178 2.05303 2.4749 4.66865 2.4749 7.79053V13.528C2.44678 13.8093 2.39053 13.9499 2.33428 14.0343L1.7999 14.9343C1.63115 15.2155 1.63115 15.553 1.7999 15.8343C1.96865 16.0874 2.2499 16.2562 2.55928 16.2562H8.38115V16.8749C8.38115 17.2124 8.6624 17.5218 9.02803 17.5218C9.36553 17.5218 9.6749 17.2405 9.6749 16.8749V16.2562H15.4687C15.778 16.2562 16.0593 16.0874 16.228 15.8343C16.3968 15.553 16.3968 15.2155 16.1999 14.9343ZM3.23428 14.9905L3.43115 14.653C3.5999 14.3718 3.68428 14.0343 3.74053 13.6405V7.79053C3.74053 5.31553 5.70928 3.23428 8.3249 2.95303C9.92803 2.78428 11.503 3.2624 12.6562 4.2749C13.6687 5.1749 14.2312 6.38428 14.2312 7.67803V13.528C14.2312 13.9499 14.3437 14.3437 14.5968 14.7374L14.7655 14.9905H3.23428Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+                
+                <!-- Notification Dropdown -->
+                <div
+                  x-show="dropdownOpen"
+                  @click.outside="dropdownOpen = false"
+                  class="absolute right-0 mt-2 w-72 rounded-lg border border-gray-300 bg-white shadow-lg dark:border-strokedark dark:bg-boxdark"
+                  x-transition
+                >
+                  <div class="px-4 py-3">
+                    <h5 class="text-sm font-medium text-bodydark2">Notifications</h5>
+                  </div>
+                  <ul class="max-h-56 overflow-y-auto">
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm hover:bg-gray-200 dark:hover:bg-meta-4"
                       >
-                        <span
-                          class="absolute -z-1 inline-flex h-full w-full animate-ping rounded-full bg-meta-1 opacity-75"
-                        ></span>
-                      </span>
-
-                      <svg
-                        class="fill-current duration-300 ease-in-out"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 18 18"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+                        New message received
+                        <span class="block text-xs text-gray-500">12 May, 2025</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm hover:bg-gray-200 dark:hover:bg-meta-4"
                       >
-                        <path
-                          d="M16.1999 14.9343L15.6374 14.0624C15.5249 13.8937 15.4687 13.7249 15.4687 13.528V7.67803C15.4687 6.01865 14.7655 4.47178 13.4718 3.31865C12.4312 2.39053 11.0812 1.7999 9.64678 1.6874V1.1249C9.64678 0.787402 9.36553 0.478027 8.9999 0.478027C8.6624 0.478027 8.35303 0.759277 8.35303 1.1249V1.65928C8.29678 1.65928 8.24053 1.65928 8.18428 1.6874C4.92178 2.05303 2.4749 4.66865 2.4749 7.79053V13.528C2.44678 13.8093 2.39053 13.9499 2.33428 14.0343L1.7999 14.9343C1.63115 15.2155 1.63115 15.553 1.7999 15.8343C1.96865 16.0874 2.2499 16.2562 2.55928 16.2562H8.38115V16.8749C8.38115 17.2124 8.6624 17.5218 9.02803 17.5218C9.36553 17.5218 9.6749 17.2405 9.6749 16.8749V16.2562H15.4687C15.778 16.2562 16.0593 16.0874 16.228 15.8343C16.3968 15.553 16.3968 15.2155 16.1999 14.9343ZM3.23428 14.9905L3.43115 14.653C3.5999 14.3718 3.68428 14.0343 3.74053 13.6405V7.79053C3.74053 5.31553 5.70928 3.23428 8.3249 2.95303C9.92803 2.78428 11.503 3.2624 12.6562 4.2749C13.6687 5.1749 14.2312 6.38428 14.2312 7.67803V13.528C14.2312 13.9499 14.3437 14.3437 14.5968 14.7374L14.7655 14.9905H3.23428Z"
-                          fill=""
-                        />
-                      </svg>
-                    </a>
-
-                    <!-- Dropdown Start -->
-                    <div
-                      x-show="dropdownOpen"
-                      class="absolute -right-27 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80"
-                    >
-                      <div class="px-4.5 py-3">
-                        <h5 class="text-sm font-medium text-bodydark2">Notification</h5>
-                      </div>
-
-                      <ul class="flex h-auto flex-col overflow-y-auto">
-                        <li>
-                          <a
-                            class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                            href="#"
-                          >
-                            <p class="text-sm">
-                              <span class="text-black dark:text-white"
-                                >Edit your information in a swipe</span
-                              >
-                              Sint occaecat cupidatat non proident, sunt in culpa qui
-                              officia deserunt mollit anim.
-                            </p>
-
-                            <p class="text-xs">12 May, 2025</p>
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                            href="#"
-                          >
-                            <p class="text-sm">
-                              <span class="text-black dark:text-white"
-                                >It is a long established fact</span
-                              >
-                              that a reader will be distracted by the readable.
-                            </p>
-
-                            <p class="text-xs">24 Feb, 2025</p>
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                            href="#"
-                          >
-                            <p class="text-sm">
-                              <span class="text-black dark:text-white"
-                                >There are many variations</span
-                              >
-                              of passages of Lorem Ipsum available, but the majority have
-                              suffered
-                            </p>
-
-                            <p class="text-xs">04 Jan, 2025</p>
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            class="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
-                            href="#"
-                          >
-                            <p class="text-sm">
-                              <span class="text-black dark:text-white"
-                                >There are many variations</span
-                              >
-                              of passages of Lorem Ipsum available, but the majority have
-                              suffered
-                            </p>
-
-                            <p class="text-xs">01 Dec, 2024</p>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <!-- Dropdown End -->
-                  </li>
-                  <!-- Notification Menu Area -->
+                        System maintenance alert
+                        <span class="block text-xs text-gray-500">24 Feb, 2025</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm hover:bg-gray-200 dark:hover:bg-meta-4"
+                      >
+                        Update available for download
+                        <span class="block text-xs text-gray-500">04 Jan, 2025</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-                <button @click="isOpen = !isOpen" class="realtive z-10 w-14 h-14 rounded-full overflow-hidden border-4 border-gray-700 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
-                <img src="../upload/images/<?php echo $row['id_photo']; ?>" alt="User Image" class="w-14 h-14 rounded-full">
+
+              <!-- Profile Dropdown -->
+              <div x-data="{ isOpen: false }" class="relative">
+                <button
+                  @click="isOpen = !isOpen"
+                  class="relative z-10 w-12 h-12 rounded-full overflow-hidden border-2 border-gray-700 hover:border-gray-300 focus:outline-none"
+                >
+                  <img
+                    src="../upload/images/<?php echo $row['id_photo']; ?>"
+                    alt="User Profile"
+                    class="w-full h-full object-cover"
+                  />
                 </button>
                 <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
-                <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
-                    <a href="../index.php" class="block px-4 py-2 account-link hover:text-white">Home</a>
-                    <a href="../alumni/alumni_settings.php" class="block px-4 py-2 account-link hover:text-white">Settings</a>
-                    <a href="../logout.php" class="block px-4 py-2 account-link hover:text-white">Sign Out</a>
+                <div
+                  x-show="isOpen"
+                  @click.outside="isOpen = false"
+                  class="absolute right-0 mt-2 w-48 rounded-lg border border-gray-300 bg-white shadow-lg dark:border-strokedark dark:bg-boxdark"
+                  x-transition
+                  >
+                  <a
+                    href="../index.php"
+                    class="block px-4 py-2 text-sm hover:bg-gray-200 dark:hover:bg-meta-4"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="../alumni/alumni_settings.php"
+                    class="block px-4 py-2 text-sm hover:bg-gray-200 dark:hover:bg-meta-4"
+                  >
+                    Settings
+                  </a>
+                  <a
+                    href="../logout.php"
+                    class="block px-4 py-2 text-sm hover:bg-gray-200 dark:hover:bg-meta-4"
+                  >
+                    Sign Out
+                  </a>
                 </div>
+              </div>
             </div>
+
+
         </header>
 
         <!-- Mobile Header & Nav -->

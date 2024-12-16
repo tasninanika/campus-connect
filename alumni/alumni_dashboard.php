3f5e9522e5548c347003cd37cd78d1308566321c
anikatasnin <jarintasnin09@gmail.com>
2024-12-16 23:50:46 +0000

    updated alumni_dashboard.php

--- alumni/alumni_dashboard.php
+++ alumni/alumni_dashboard.php
@@ -759,6 +759,13 @@
     
           <!-- Recent Activities -->
           <div class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+          <?php
+							$a=$_SESSION['alumni_id'];
+							
+							$result = mysqli_query($db,"SELECT * FROM job,alumni WHERE job.u_id=alumni.alumni_id");
+							
+							while($row = mysqli_fetch_array($result)) {
+							?>
             <div class="rounded-t mb-0 px-0 border-0">
               <div class="block w-full">
                 <div class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
@@ -767,7 +774,7 @@
                 <ul class="my-1">
                   <li class="flex px-4">
                     <div class="w-9 h-9 rounded-full flex-shrink-0 bg-indigo-500 my-2 mr-3">
-                      <svg class="w-9 h-9 fill-current text-indigo-50" viewBox="0 0 36 36"><path d="M18 10c-4.4 0-8 3.1-8 7s3.6 7 8 7h.6l5.4 2v-4.4c1.2-1.2 2-2.8 2-4.6 0-3.9-3.6-7-8-7zm4 10.8v2.3L18.9 22H18c-3.3 0-6-2.2-6-5s2.7-5 6-5 6 2.2 6 5c0 2.2-2 3.8-2 3.8z"></path></svg>
+                    <img src="../upload/images/<?php echo $row["logo"]; ?>" alt="User Image">
                     </div>
                     <div class="flex-grow flex items-center border-b border-gray-100 dark:border-gray-400 text-sm text-gray-600 dark:text-gray-100 py-2">
                       <div class="flex-grow flex justify-between items-center">
@@ -798,6 +805,9 @@
                         </div>
                       </div>
                     </div>
+                    <?php
+                }
+                ?>
                   </li>
                 </ul>
                 <div class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">

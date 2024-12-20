70c6cb1943454b1adb0d3b89482c981c9801f0b7
anikatasnin <jarintasnin09@gmail.com>
2024-12-20 21:17:46 +0000

    updated posted_job.php

--- alumni/posted_job.php
+++ alumni/posted_job.php
@@ -680,24 +680,41 @@
                     $created_at = $rows['created_at']; ?>
                         <div class="grid grid-cols-1 sm:grid-cols-2 gap-10">
                             <!-- Card 1 -->
-                            <div class="border-2 border-gray-400 bg-slate-200 rounded-lg p-4 relative">
+                            <div class="border-2 border-gray-400 bg-slate-200 rounded-lg p-4 relative dark:bg-gray-700">
                                 <!-- Card Content with Flexbox -->
                                 <div class="flex items-center space-x-4 flex-row-reverse">
                                     <!-- Image -->
                                     <img src="../upload/images/<?php echo $logo; ?>" alt="Logo" class="w-16 h-16 object-cover rounded-full -mt-20">
                                     <!-- Title and Details -->
                                     <div>
-                                        <h2 class="text-xl font-semibold text-black dark:text-white leading-tight break-words">
+                                        <h2 class="text-xl font-semibold text-blue-900 dark:text-white leading-tight break-words">
                                             <?php echo $job_title; ?>
                                         </h2>
-                                        <p class="mt-2 font-bold"><?php echo $company_name; ?></p>
-                                        <p class="text-gray-700 mt-2 text-base"><?php echo $location; ?></p>
-                                        <p class="text-gray-700 mt-2 text-base"><?php echo $experience; ?></p>
+                                        <p class="mt-2 font-bold dark:text-white"><?php echo $company_name; ?></p>
+                                        <div class="text-gray-700 mt-2 text-base dark:text-white flex flex-col gap-2">
+                                        <!-- First Icon with Text -->
+                                        <div class="flex items-center gap-2">
+                                          <svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 384 512">
+                                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                            <path fill="#808285" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+                                          </svg>
+                                          <span><?php echo $location; ?></span>
+                                        </div>
+
+                                        <!-- Second Icon with Text -->
+                                        <div class="flex items-center gap-2">
+                                          <svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 576 512">
+                                            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                            <path fill="#7c8083" d="M256 0l64 0c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32zM64 64l128 0 0 48c0 26.5 21.5 48 48 48l96 0c26.5 0 48-21.5 48-48l0-48 128 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 128C0 92.7 28.7 64 64 64zM176 437.3c0 5.9 4.8 10.7 10.7 10.7l202.7 0c5.9 0 10.7-4.8 10.7-10.7c0-29.5-23.9-53.3-53.3-53.3l-117.3 0c-29.5 0-53.3 23.9-53.3 53.3zM288 352a64 64 0 1 0 0-128 64 64 0 1 0 0 128z" />
+                                          </svg>
+                                          <span><?php echo $experience; ?></span>
+                                        </div>
+                                      </div>
                                     </div>
                                 </div>
                                 <!-- Date (bottom right corner) -->
-                                <span class="absolute bottom-4 right-4 text-sm">
-                                    <?php echo date('d M, Y', strtotime($created_at)); ?>
+                                <span class="absolute bottom-4 right-4 text-sm dark:text-white flex items-center space-x-2 gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="14" width="12.25" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#82878c" d="M128 0c17.7 0 32 14.3 32 32l0 32 128 0 0-32c0-17.7 14.3-32 32-32s32 14.3 32 32l0 32 48 0c26.5 0 48 21.5 48 48l0 48L0 160l0-48C0 85.5 21.5 64 48 64l48 0 0-32c0-17.7 14.3-32 32-32zM0 192l448 0 0 272c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 192zm80 64c-8.8 0-16 7.2-16 16l0 96c0 8.8 7.2 16 16 16l96 0c8.8 0 16-7.2 16-16l0-96c0-8.8-7.2-16-16-16l-96 0z"/></svg><?php echo date('d M, Y', strtotime($created_at)); ?>
                                 </span>
                             </div>
                         </div>

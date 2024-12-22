99ad303f8e349991da2b51adf1f6e48c6e205c84
anikatasnin <jarintasnin09@gmail.com>
2024-12-22 00:18:25 +0000

    updated update_job.php.php

--- alumni/update_job.php
+++ alumni/update_job.php
@@ -4,7 +4,7 @@ include("../db_con/dbCon.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve user ID from session
-    $job_id = $_SESSION['job_id'];
+    $alumni_id = $_SESSION['alumni_id'];
 
     // Get POST data and sanitize
     $job_title = mysqli_real_escape_string($db, $_POST['title']);
@@ -20,30 +20,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $com_description = mysqli_real_escape_string($db, $_POST['com_description']);
     $apply_info = mysqli_real_escape_string($db, $_POST['apply_info']);
 
-    // Create SQL query to update user information
+    // Create SQL query to update job information
     $sql = "UPDATE job
-            ON job.u_id = alumni.alumni_id
-            SET job.title = '$job_title',
-                job.type = '$type',
-                job.category = '$category',
-                job.department = '$department',
-                job.company_name = '$company_name',
-                job.experience = '$experience',
-                job.qualification = '$qualification',
-                job.location = '$location',
-                job.salary = '$salary'
-                job.description = '$description'
-                job.com_description = '$com_description'
-                job.apply_info = '$apply_info'
-            WHERE job.u_id = '$u_id'";
+            SET title = '$job_title',
+                type = '$type',
+                category = '$category',
+                department = '$department',
+                company_name = '$company_name',
+                experience = '$experience',
+                qualification = '$qualification',
+                location = '$location',
+                salary = '$salary',
+                description = '$description',
+                com_description = '$com_description',
+                apply_info = '$apply_info'
+            WHERE u_id = '$alumni_id'";
 
     // Execute the query
-    $result=mysqli_query($db, $sql) or die(mysqli_error ($db));
-    if($result==1)
-    {
-            header("location:alumni_job.php");
-            
-        
+    $result = mysqli_query($db, $sql) or die(mysqli_error($db));
+
+    if ($result) {
+        // Redirect on success
+        header("Location: posted_job.php");
+        exit;
+    } else {
+        // Display error message
+        echo "Error updating job information.";
     }
 }
 ?>

259b8154a000c46b5c5fd346e63e45facb58db59
anikatasnin <jarintasnin09@gmail.com>
2024-12-26 21:57:34 +0000

    updated posted_resources.php

---
+++ alumni/update_resources.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include("../db_con/dbCon.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve user ID from session
+    $alumni_id = $_SESSION['alumni_id'];
+    $material_id = mysqli_real_escape_string($db, $_POST['material_id']);
+
+    // Get POST data and sanitize
+    $resources_title = mysqli_real_escape_string($db, $_POST['title']);
+    $type = mysqli_real_escape_string($db, $_POST['type']);
+    $description = mysqli_real_escape_string($db, $_POST['content']);
+
+    // Create SQL query to update job information
+    $sql = "UPDATE resources
+            SET title = '$resources_title',
+                type = '$type',
+                content = '$description'
+             WHERE material_id = '$material_id' AND u_id = '$alumni_id'";
+
+    // Execute the query
+    $result = mysqli_query($db, $sql) or die(mysqli_error($db));
+
+    if ($result) {
+        // Redirect on success
+        header("Location: posted_resources.php");
+        exit;
+    } else {
+        // Display error message
+        echo "Error updating resources information.";
+    }
+}
+?>

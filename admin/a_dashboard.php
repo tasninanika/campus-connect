d11b0013121fea44506821bbf07459f2ba2b3130
anikatasnin <jarintasnin09@gmail.com>
2024-12-29 12:42:30 +0000

    updated a_dashboard.php

--- admin/a_dashboard.php
+++ admin/a_dashboard.php
@@ -659,11 +659,6 @@
           <!-- Recent Activities part1 -->
           <div class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
             <div class="rounded-t mb-0 px-0 border-0">
-              <!-- <div class="flex flex-wrap items-center px-4 py-2">
-                <div class="relative w-full max-w-full flex-grow flex-1">
-                  <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Recent Activities</h3>
-                </div>
-              </div> -->
             <div class="block w-full overflow-x-auto">
               <div class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                  Alumni Requests
@@ -718,7 +713,7 @@
                                             </h3>
                                             <?php echo $displayDate; ?>
                                         </div>
-                                        <div class="flex-shrink-0 ml-2 mt-">
+                                        <div class="flex-shrink-0 ml-2 mt-6">
                                             <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500" href="pending_alumni.php" style="outline: none;">
                                             Details<span><svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" class="transform transition-transform duration-500 ease-in-out"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></span>
                                             </a>
@@ -805,8 +800,8 @@
                                       <?php endif; ?>
                                       <?php echo $displayDate; ?>
                                     </div>
-                                    <div class="flex-shrink-0 ml-2 mt-5">
-                                      <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500" href="pending_blogs.php" style="outline: none;">
+                                    <div class="flex-shrink-0 ml-2">
+                                      <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500 mt-12" href="pending_blogs.php" style="outline: none;">
                                             Details<span><svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" class="transform transition-transform duration-500 ease-in-out"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></span>
                                       </a>
                                     </div>
@@ -986,7 +981,7 @@
                                       <p><?php echo $displayDate; ?></p>
                                     </div>
                                     <div class="flex-shrink-0 ml-2">
-                                      <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500" href="posted_resources.php" style="outline: none;"> Details<span><svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" class="transform transition-transform duration-500 ease-in-out"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></span>
+                                      <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500 mt-16" href="posted_resources.php" style="outline: none;"> Details<span><svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" class="transform transition-transform duration-500 ease-in-out"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></span>
                                       </a>
                                     </div>
                                   </div>

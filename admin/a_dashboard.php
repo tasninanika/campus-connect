00fa357c8dc57ae74e978ec3ed6504ceb50b8892
anikatasnin <jarintasnin09@gmail.com>
2024-12-29 10:19:03 +0000

    updated a_dashboard.php

--- admin/a_dashboard.php
+++ admin/a_dashboard.php
@@ -669,47 +669,83 @@
                  Requests
                 </div>
                 <?php
-								include_once '../db_con/dbCon.php';
-								$result = mysqli_query($db,"SELECT * FROM user INNER JOIN alumni ON user.u_id=alumni.alumni_id AND user.status = 'Pending'");
+                include_once '../db_con/dbCon.php';
+                $result = mysqli_query($db, "SELECT * FROM user INNER JOIN alumni ON user.u_id=alumni.alumni_id AND user.status = 'Pending'");
+                // Ensure PHP timezone is set
+                date_default_timezone_set('Asia/Dhaka');
+
+                // Fetch rows from the database
                 if (mysqli_num_rows($result) > 0) {
-								while($row = mysqli_fetch_array($result)) {
-							?>
-                            <ul class="my-1">
-                              <li class="flex px-4">
+                    while ($row = mysqli_fetch_array($result)) {
+                        if (isset($row['created_at']) && !is_null($row['created_at'])) {
+                            $posted_date = $row['created_at'];
+
+                            // Convert database UTC time to Asia/Dhaka time
+                            $postedDate = new DateTime($posted_date, new DateTimeZone('UTC'));
+                            $postedDate->setTimezone(new DateTimeZone('Asia/Dhaka'));
+
+                            $currentDate = new DateTime('now', new DateTimeZone('Asia/Dhaka'));
+
+                            // Calculate total difference in seconds
+                            $totalSeconds = $currentDate->getTimestamp() - $postedDate->getTimestamp();
+
+                            if ($totalSeconds >= 30 * 24 * 60 * 60) { // More than 30 days
+                                $displayDate = "30+ days ago";
+                            } elseif ($totalSeconds >= 24 * 60 * 60) { // More than 1 day
+                                $days = floor($totalSeconds / (24 * 60 * 60));
+                                $displayDate = $days === 1 ? "1 day ago" : "{$days} days ago";
+                            } elseif ($totalSeconds >= 60 * 60) { // More than 1 hour
+                                $hours = floor($totalSeconds / (60 * 60));
+                                $displayDate = $hours === 1 ? "1 hour ago" : "{$hours} hours ago";
+                            } elseif ($totalSeconds >= 60) { // More than 1 minute
+                                $minutes = floor($totalSeconds / 60);
+                                $displayDate = $minutes === 1 ? "1 minute ago" : "{$minutes} minutes ago";
+                            } else { // Less than 1 minute
+                                $displayDate = "Just now";
+                            }
+                        }                                        
+                ?>
+
+                        <ul class="my-1">
+                            <li class="flex px-4">
                                 <div class="flex-shrink-0 my-2 mr-3">
-                                  <img src="../upload/images/<?php echo $row["id_photo"]; ?>" alt="User Image" class="h-9 w-9 rounded-full">
+                                    <img src="../upload/images/<?php echo $row["id_photo"]; ?>" alt="User Image" class="h-9 w-9 rounded-full">
                                 </div>
                                 <div class="flex-grow flex items-center border-gray-100 dark:border-gray-400 text-sm text-gray-600 dark:text-gray-100 py-2">
-                                  <div class="flex-grow flex justify-between items-center">
-                                    <div class="self-center">
-                                      <h3 class="font-medium text-gray-800 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-100" style="outline: none;"><?php echo $row["title"]; ?></h3>
-                                      <p class="line-clamp-2"><?php echo $row["description"]; ?></p>
-                                      <a href="description.php?blog_id=<?php echo $row['blog_id']; ?>" 
-                                        class="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500 text-sm font-medium mt-1 block">
-                                        Read More
-                                      </a>
-                                      <?php echo date('d M, Y', strtotime($row["created_at"])); ?>
+                                    <div class="flex-grow flex justify-between items-center">
+                                        <div class="self-center">
+                                            <h3 class="font-medium text-gray-800 hover:text-gray-900 dark:text-gray-50 dark:hover:text-gray-100" style="outline: none;">
+                                                <span class="font-extrabold"><?php echo $row["first_Name"]; ?> <?php echo $row["last_Name"]; ?></span> wants to join our community.
+                                            </h3>
+                                            <?php echo $displayDate; ?>
+                                        </div>
+                                        <div class="flex-shrink-0 ml-2 mt-20">
+                                            <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500" href="posted_blogs.php" style="outline: none;">
+                                                <span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 512 512" class="mr-1">
+                                                        <path fill="#B197FC" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/>
+                                                    </svg>
+                                                </span>
+                                                Edit
+                                            </a>
+                                        </div>
                                     </div>
-                                    <div class="flex-shrink-0 ml-2 mt-20">
-                                      <a class="flex items-center font-medium text-purple-500 hover:text-blue-600 dark:text-purple-400 dark:hover:text-blue-500" href="posted_blogs.php" style="outline: none;"><span><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 512 512" class="mr-1"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#B197FC" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg></span>
-                                        Edit
-                                      </a>
-                                    </div>
-                                  </div>
                                 </div>
-                              </li>
-                            </ul>
-                            <?php
-                        }
-                    } else {
-                        // If no job posts exist, display a message
-                        ?>
-                        <div class="text-center py-4 text-gray-500 dark:text-gray-400">
-                            <p>No posts available. Create a new post to get started.</p>
-                        </div>
-                        <?php
+                            </li>
+                        </ul>
+
+                <?php
                     }
+                } else {
+                    // If no job posts exist, display a message
                     ?>
+                    <div class="text-center py-4 text-gray-500 dark:text-gray-400">
+                        <p>No posts available. Create a new post to get started.</p>
+                    </div>
+                    <?php
+                }
+                ?>
+
               </div>
             </div>
           </div>

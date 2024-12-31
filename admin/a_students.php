bdf8e2787e47d3296cbfe23c6437ad2222d60340
anikatasnin <jarintasnin09@gmail.com>
2024-12-31 20:15:51 +0600

    updated reject_blog.php

--- admin/a_students.php
+++ admin/a_students.php
@@ -360,7 +360,7 @@
     </aside> 
     <div class="w-full flex flex-col h-screen overflow-y-auto overflow-x-hidden dark:bg-gray-900">
         <!-- Desktop Header -->
-        <header class="sticky top-0 z-999 w-full items-center bg-white dark:bg-gray-800 shadow pt-5 px-6  hidden sm:flex">
+        <header class="sticky top-0 z-999 w-full items-center bg-white dark:bg-gray-800 shadow py-2 px-6  hidden sm:flex">
             <div class="w-1/2">
             <h1 class="mb-36 lg:mb-0 -ml-10 lg:-ml-0 lg:text-4xl font-garamond flex dark:text-white multicolor-text"><img src="images/logo (2).png" alt="" class="lg:w-14 w-14 -mt-1"></h1>
             </div>
@@ -408,109 +408,6 @@
                 </svg>
               </button>
             </div>
-        <!-- Notification Dropdown -->
-        <div x-data="{ 
-            dropdownOpen: false, 
-            unreadCount: 0,
-            fetchUnreadCount() {
-                fetch('check_unread.php')
-                    .then(res => res.json())
-                    .then(data => {
-                        this.unreadCount = data.unread_count;
-                    });
-            },
-            markAsRead() {
-                fetch('mark_as_read.php', { method: 'POST' })
-                    .then(() => {
-                        this.unreadCount = 0; // Red dot hide
-                    });
-            }
-            }" x-init="fetchUnreadCount(); setInterval(fetchUnreadCount, 5000)" class="relative">
-            <a
-                class="relative flex h-9 w-9 items-center justify-center rounded-full top-2 border border-gray-300 bg-gray-100 hover:text-primary dark:border-strokedark dark:bg-meta-4 dark:text-white"
-                href="#"
-                @click.prevent="dropdownOpen = !dropdownOpen; notifying = false"
-            >
-            
-                <!-- Notification Bell -->
-                <a href="#" 
-                @click.prevent="dropdownOpen = !dropdownOpen; markAsRead();" 
-                class="relative flex h-9 w-9 -top-7 items-center justify-center rounded-full border border-gray-300 bg-gray-100 hover:text-primary dark:border-strokedark dark:bg-meta-4 dark:text-white">
-
-                  <!-- Notification Dot (Single Red Dot with Animation) -->
-                  <span 
-                      x-show="unreadCount > 0"
-                      class="absolute -top-1 right-0 z-10 h-2 w-2 rounded-full bg-red-500"
-                  >
-                      <span 
-                          class="absolute -z-1 inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"
-                      ></span>
-                  </span>
-
-                  <!-- Bell Icon -->
-                  <svg
-                      class="fill-current duration-300 ease-in-out"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 18 18"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                      <path
-                          d="M16.1999 14.9343L15.6374 14.0624C15.5249 13.8937 15.4687 13.7249 15.4687 13.528V7.67803C15.4687 6.01865 14.7655 4.47178 13.4718 3.31865C12.4312 2.39053 11.0812 1.7999 9.64678 1.6874V1.1249C9.64678 0.787402 9.36553 0.478027 8.9999 0.478027C8.6624 0.478027 8.35303 0.759277 8.35303 1.1249V1.65928C8.29678 1.65928 8.24053 1.65928 8.18428 1.6874C4.92178 2.05303 2.4749 4.66865 2.4749 7.79053V13.528C2.44678 13.8093 2.39053 13.9499 2.33428 14.0343L1.7999 14.9343C1.63115 15.2155 1.63115 15.553 1.7999 15.8343C1.96865 16.0874 2.2499 16.2562 2.55928 16.2562H8.38115V16.8749C8.38115 17.2124 8.6624 17.5218 9.02803 17.5218C9.36553 17.5218 9.6749 17.2405 9.6749 16.8749V16.2562H15.4687C15.778 16.2562 16.0593 16.0874 16.228 15.8343C16.3968 15.553 16.3968 15.2155 16.1999 14.9343ZM3.23428 14.9905L3.43115 14.653C3.5999 14.3718 3.68428 14.0343 3.74053 13.6405V7.79053C3.74053 5.31553 5.70928 3.23428 8.3249 2.95303C9.92803 2.78428 11.503 3.2624 12.6562 4.2749C13.6687 5.1749 14.2312 6.38428 14.2312 7.67803V13.528C14.2312 13.9499 14.3437 14.3437 14.5968 14.7374L14.7655 14.9905H3.23428Z"
-                          fill="purple"
-                      />
-                  </svg>
-              </a>
-
-
-            <!-- Notification Dropdown -->
-            <div
-                x-show="dropdownOpen"
-                @click.outside="dropdownOpen = false"
-                class="absolute right-0 mt-2 w-80 rounded-lg border border-gray-400 bg-white shadow-lg dark:border-gray-700 dark:bg-gray-800"
-                x-transition
-                >
-                <!-- Header -->
-                <div class="px-4 py-3 border-b border-gray-200 dark:border-gray-700">
-                    <h5 class="text-sm font-semibold text-gray-800 dark:text-gray-200">
-                        Notifications
-                    </h5>
-                </div>
-
-                <!-- Notification List -->
-                <ul class="max-h-56 overflow-y-auto divide-y divide-gray-200 dark:divide-gray-700">
-                    <?php 
-                    $sql = "SELECT * FROM announcement ORDER BY created_at DESC LIMIT 3";
-                    $query = mysqli_query($db, $sql);
-
-                    if(mysqli_num_rows($query) > 0) {
-                        while ($rows = mysqli_fetch_assoc($query)) {
-                            $announcement_title = $rows['title'];
-                            $posted_date = $rows['created_at']; 
-                    ?>
-                        <li>
-                            <a
-                                href="#"
-                                class="block px-4 py-3 transition-all hover:bg-gray-100 dark:hover:bg-gray-700"
-                            >
-                                <p class="text-sm font-medium text-gray-700 dark:text-gray-200">
-                                    <?php echo htmlspecialchars($announcement_title); ?>
-                                </p>
-                                <span class="block text-xs text-gray-500 dark:text-gray-400">
-                                    <?php echo date('d M, Y', strtotime($posted_date)); ?>
-                                </span>
-                            </a>
-                        </li>
-                    <?php
-                        }
-                    } else { ?>
-                        <li class="px-4 py-3 text-sm text-gray-500 dark:text-gray-400">
-                            No new notifications.
-                        </li>
-                    <?php } ?>
-                </ul>
-            </div>
-        </div>
         <!-- Profile Dropdown -->
         <div x-data="{ isOpen: false }" class="relative">
                 <button

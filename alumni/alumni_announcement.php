9bb94adbfc6c8decbc560b4bc20047bdc623cc4a
anikatasnin <jarintasnin09@gmail.com>
2025-01-02 11:23:14 +0600

    updated alumni_announcement.php

--- alumni/alumni_announcement.php
+++ alumni/alumni_announcement.php
@@ -704,15 +704,15 @@
                         <!-- 1 -->
                         <li class="border-l-2 border-[#797DFC]">
                             <div class="md:flex flex-start">
-                            <div class="bg-[#797DFC] w-10 h-6 flex items-center justify-center rounded-full -ml-3">
+                            <div class="bg-[#797DFC] w-7 h-7 flex items-center justify-center rounded-full -ml-3.5">
                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" class="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                 <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
                                 </svg>
                             </div>
-                            <div class="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-screen ml-6 mb-10 dark:bg-gray-800">
+                            <div class="block p-6 rounded-lg shadow-lg bg-gray-100 w-full h-[160px] ml-6 mb-10 dark:bg-gray-800">
                                 <div class="flex justify-between mb-4">
-                                <h3 class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm"><?php echo $announcement_title; ?></h3>
-                                <h3 class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm dark:text-white"><?php echo $posted_date; ?></h3>
+                                <h3 class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 text-sm"><?php echo $announcement_title; ?></h3>
+                                <h3 class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 text-sm dark:text-white"><?php echo $posted_date; ?></h3>
                                 </div>
                                 <p class="text-gray-700 mb-6 dark:text-white"><?php echo $content; ?></p>
                             </div>
